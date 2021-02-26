f048e1eeb0c9ec8604be6d0ccb96c2ffcbe5da99
Kelly Yong <kelly.a.yong@gmail.com>
2021-02-26 17:23:01 -0500

    Write functions for operations and add event listeners to number buttons

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 
-// functions
+// operation functions
 function add(a,b) {
-    a+b;
+    let total = a+b;
+    return total;
+}
+function minus(a,b) {
+    let total = a-b;
+    return total;
+} 
+function times(a,b) {
+    let total = a*b;
+    return total;
+} 
+function divide(a,b) {
+    let total = a/b;
+    return total;
+} 
+
+// Numbers to be operated on
+let firstNumber = '';
+let nextNumber = '';
+let numberHolder = '';
+
+// DOM - number buttons
+const numbers = document.querySelectorAll('.number-btn');
+numbers.forEach(number => {
+    number.addEventListener('click', concatNumbers)
+});
+
+function concatNumbers(e) {
+    numberHolder += + e.target.innerText;
+    console.log(numberHolder);
+    console.log(e.target.innerText);
+    return numberHolder;
 }
