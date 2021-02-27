ab819ff38b283955588d23810212319fd4518257
Kelly Yong <kelly.a.yong@gmail.com>
2021-02-27 02:24:06 -0500

    Fix add() and minus() functions and create clear()

--- index.js
+++ index.js
@@ -1,37 +1,44 @@
-let total = 0;
+let total = '';
 // operation functions
 function add(a,b) {
+    a = Number(a);
+    b = Number(b);
     total = a+b;
+    console.log(a+'+'+b);
     console.log('total: ', total);
     return total;
 }
 function minus(a,b) {
+    a = Number(a);
+    b = Number(b);
     total = a-b;
+    console.log(a+'-'+b);
     console.log('total: ', total);
     return total;
 } 
 function times(a,b) {
+    a = Number(a);
+    b = Number(b);
     total = a*b;
+    console.log(a+'*'+b);
     console.log('total: ', total);
     return total;
 } 
 function divide(a,b) {
+    a = Number(a);
+    b = Number(b);
     total = a/b;
+    console.log(a+'/'+b);
     console.log('total: ', total);
     return total;
 } 
-
-// Numbers to be operated on
-let firstNumber = '';
-let nextNumber = '';
-let displayNumber = '';
 
 // function - update display number
 function updateDisplay() {
     const divDisplay = document.querySelector('#divDisplay');
     const paraDisplay = document.querySelector('#paraDisplay');
     paraDisplay.innerText = displayNumber;
-    console.log('paraDisplay.innerText : ', paraDisplay.innerText);
+    console.log('displayNumber: ', paraDisplay.innerText);
     divDisplay.appendChild(paraDisplay);
 }
 // function - update display number
@@ -39,14 +46,31 @@ function displayTotal() {
     const divDisplay = document.querySelector('#divDisplay');
     const paraDisplay = document.querySelector('#paraDisplay');
     paraDisplay.innerText = total;
-    console.log('paraDisplay.innerText : ', paraDisplay.innerText);
+    console.log('total: ', paraDisplay.innerText);
     divDisplay.appendChild(paraDisplay);
 }
 
+// DOM - clear button
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', clearDisplay);
+
 // function - clear display
 function clearDisplay() {
-
+    const divDisplay = document.querySelector('#divDisplay');
+    const paraDisplay = document.querySelector('#paraDisplay');
+    firstNumber = '';
+    secondNumber = '';
+    displayNumber = '';
+    paraDisplay.innerText = displayNumber;
+    console.log('paraDisplay.innerText=displayNumber: ', paraDisplay.innerText);
+    divDisplay.appendChild(paraDisplay);
+    operator = 'plus';
 }
+
+// Numbers to be operated on
+let firstNumber = '';
+let secondNumber = '';
+let displayNumber = '';
 
 // DOM - number buttons
 const numbers = document.querySelectorAll('.number-btn');
@@ -55,9 +79,9 @@ numbers.forEach(number => {
 });
 
 function concatNumbers(e) {
-    displayNumber += + e.target.innerText;
-    console.log('displayNumber: '+displayNumber);
+    displayNumber = displayNumber + e.target.innerText;
     updateDisplay();
+    console.log(updateDisplay());
     return displayNumber;
 }
 
@@ -76,40 +100,74 @@ function selectOperator(e) {
     console.log('operator: '+operator);
     console.log('newOperator: ', newOperator);
     operate();
-    firstNumber = displayNumber;
-    displayNumber = 0;
+    console.log(operate());
 }
 
 // operate function
 function operate() {
     switch (operator) {
         case 'plus':
-            add(firstNumber,displayNumber);
+            console.log('case:plus');
+            console.log('1st#,2nd: '+firstNumber,secondNumber);
+            secondNumber = displayNumber;
+            add(firstNumber,secondNumber);
+            displayTotal();
+            firstNumber = total;
+            displayNumber = '';
             if(newOperator!=='equal') {
                 operator = newOperator;
             };
+            console.log('op,newOp: '+operator,newOperator);
             break;
         case 'minus':
-            minus(firstNumber,displayNumber);
+            console.log('case:minus');
+            console.log('1st#,2nd: '+firstNumber,secondNumber);
+            secondNumber = displayNumber;
+            minus(firstNumber,secondNumber);
+            displayTotal();
+            firstNumber = total;
+            displayNumber = '';
             if(newOperator!=='equal') {
                 operator = newOperator;
             };
+            console.log('op,newOp: '+operator,newOperator);
             break;
         case 'times':
-            times(firstNumber,displayNumber);
+            console.log('case:times');
+            console.log('1st#,2nd: '+firstNumber,secondNumber);
+            secondNumber = displayNumber;
+            times(firstNumber,secondNumber);
+            displayTotal();
+            firstNumber = total;
+            displayNumber = '';
             if(newOperator!=='equal') {
                 operator = newOperator;
             };
+            console.log('op,newOp: '+operator,newOperator);
             break;
         case 'divide':
-            divide(firstNumber,displayNumber);
+            console.log('case:divide');
+            console.log('1st#,2nd: '+firstNumber,secondNumber);
+            secondNumber = displayNumber;
+            divide(firstNumber,secondNumber);
+            displayTotal();
+            firstNumber = total;
+            displayNumber = '';
             if(newOperator!=='equal') {
                 operator = newOperator;
             };
+            console.log('op,newOp: '+operator,newOperator);
             break;
         case 'equal':
-            firstNumber = displayNumber;
+            console.log('case:equal');
+            console.log('1st#,2nd: '+firstNumber,secondNumber);
+            secondNumber = displayNumber;
+            displayTotal();
+            firstNumber = total;
             operate();
+            console.log('op,newOp: '+operator,newOperator);
             break;
+        case '':
+            operator = newOperator;
     };
 }
