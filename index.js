14afba52c7d2418bba6d309f79f699e3a7a433ca
Kelly Yong <kelly.a.yong@gmail.com>
2021-02-26 19:14:06 -0500

    Add operate function to do operations on numbers

--- index.js
+++ index.js
@@ -2,25 +2,29 @@
 // operation functions
 function add(a,b) {
     let total = a+b;
+    console.log('total: ', total);
     return total;
 }
 function minus(a,b) {
     let total = a-b;
+    console.log('total: ', total);
     return total;
 } 
 function times(a,b) {
     let total = a*b;
+    console.log('total: ', total);
     return total;
 } 
 function divide(a,b) {
     let total = a/b;
+    console.log('total: ', total);
     return total;
 } 
 
 // Numbers to be operated on
 let firstNumber = '';
 let nextNumber = '';
-let numberHolder = '';
+let displayNumber = '';
 
 // DOM - number buttons
 const numbers = document.querySelectorAll('.number-btn');
@@ -29,8 +33,60 @@ numbers.forEach(number => {
 });
 
 function concatNumbers(e) {
-    numberHolder += + e.target.innerText;
-    console.log(numberHolder);
-    console.log(e.target.innerText);
-    return numberHolder;
+    displayNumber += + e.target.innerText;
+    console.log('displayNumber: '+displayNumber);
+    return displayNumber;
+}
+
+// operator variables - add (default), minus, times, divide
+let operator = 'plus';
+let newOperator = '';
+
+// DOM - operator buttons
+const operators = document.querySelectorAll('.operation-btn');
+operators.forEach(operator => {
+    operator.addEventListener('click', selectOperator);
+});
+
+function selectOperator(e) {
+    newOperator = e.target.id;
+    console.log('operator: '+operator);
+    console.log('newOperator: ', newOperator);
+    operate();
+    firstNumber = displayNumber;
+    displayNumber = 0;
+}
+
+// operate function
+function operate() {
+    switch (operator) {
+        case 'plus':
+            add(firstNumber,displayNumber);
+            if(newOperator!=='equal') {
+                operator = newOperator;
+            };
+            break;
+        case 'minus':
+            minus(firstNumber,displayNumber);
+            if(newOperator!=='equal') {
+                operator = newOperator;
+            };
+            break;
+        case 'times':
+            times(firstNumber,displayNumber);
+            if(newOperator!=='equal') {
+                operator = newOperator;
+            };
+            break;
+        case 'divide':
+            divide(firstNumber,displayNumber);
+            if(newOperator!=='equal') {
+                operator = newOperator;
+            };
+            break;
+        case 'equal':
+            firstNumber = displayNumber;
+            operate();
+            break;
+    };
 }
